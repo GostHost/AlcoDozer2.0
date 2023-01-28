51ab9600285ed9071059ed4da85c3f5143dc0269
GostHost <calmghost@yandex.ru>
2023-01-28 10:30:55 +0400

    Add new project

---
+++ src/main/java/com/example/AlcoDozer20/config/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.AlcoDozer20.config;
+
+//import org.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+//    @Autowired
+//    UserService userService;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
---
+++ src/main/java/com/example/AlcoDozer20/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.example.AlcoDozer20.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String start(Model model) {
+//        return "redirect:/home";
+        return "/start";
+    }
+
+    @GetMapping("/home")
+    public String home(@RequestParam(name="title", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("title", name);
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model){
+        return "about";
+    }
+
+}
