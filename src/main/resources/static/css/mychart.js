94935297f94a97604d598d8e5f425b4f6f21db5c
GostHost <calmghost@yandex.ru>
2023-02-02 12:37:33 +0400

    Add new files

--- src/main/resources/static/css/mychart.js
+++ src/main/resources/static/css/mychart.js
@@ -1,14 +1,12 @@
 var popCanvas = document.getElementById("myChart");
-Volume vol = new Volume();
-var volume = vol.getVolume();
-var date = vol.getDate();
+
 var barChart = new Chart(popCanvas, {
   type: 'bar',
   data: {
-    labels: [[${volume.volume}], "India", "United States", "Indonesia", "Brazil", "Pakistan", "Nigeria", "Bangladesh", "Russia", "Japan"],
+    labels: [[${price}], "India", "United States", "Indonesia", "Brazil", "Pakistan", "Nigeria", "Bangladesh", "Russia", "Japan"],
     datasets: [{
-      label: 'Population',
-      data: [1379302771, 1281935911, 326625791, 260580739, 207353391, 204924861, 190632261, 157826578, 142257519, 126451398],
+      label: 'Траты',
+      data: [[${price}], 1281935911, 326625791, 260580739, 207353391, 204924861, 190632261, 157826578, 142257519, 126451398],
       backgroundColor: [
         'rgb(255, 99, 132)',
         'rgb(54, 162, 235)',
